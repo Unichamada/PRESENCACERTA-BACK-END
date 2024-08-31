7bbc201faef5efc6dd1a0a5bf42861b5268ff06f
Lucas Campolim <lucascampolim@protonmail.com>
2024-08-31 09:19:47 -0400

    feat(crud): pessoa

---
+++ src/shared/interfaces/people.interface.ts
@@ -0,0 +1,18 @@
+export interface IPeople {
+  id?: number;
+  codigo: string;
+  nome: string;
+  cpf: string;
+  tipoId?: number;
+  unidadeId?: number;
+  usuarioId?: number;
+
+  /*
+  tipo?: IPeopleType;
+  unidade?: IUnit;
+  usuario?: IUser;
+  turmas?: IPeopleClass[];
+  materias?: ITeacherSubjectClass[];
+  presencas?: IPresence[];
+  */
+}
