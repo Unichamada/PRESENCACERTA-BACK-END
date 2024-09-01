f8012788f60f2f2ae453d934feb3d1ef66644617
Douglas A. da Silva <59983373+Douglastark123@users.noreply.github.com>
2024-08-31 21:10:00 -0400

    feat/douglas-adiciona-crud-materia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import MateriaModule from './modules/materia/materia.module';
 
 @Module({
-  imports: [],
+  imports: [MateriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/materia/materia.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import MateriaService from './materia.service';
+import CreateMateriaDto from './dto/create-materia.dto';
+import UpdateMateriaDto from './dto/update-materia.dto';
+
+@Controller('subject')
+export default class MateriaController {
+  constructor(private readonly subjectService: MateriaService) {}
+
+  @Post()
+  create(@Body() createMateriaDto: CreateMateriaDto) {
+    return this.subjectService.create(createMateriaDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.subjectService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.subjectService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMateriaDto: UpdateMateriaDto) {
+    return this.subjectService.update(Number(id), updateMateriaDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.subjectService.remove(Number(id));
+  }
+}
