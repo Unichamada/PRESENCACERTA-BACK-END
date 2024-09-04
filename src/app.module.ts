a83ad37fde53f0a2918c726649ad5cda67e0bb79
magnojunior07 <magnojunior319@gmail.com>
2024-09-04 00:30:40 -0400

    feat: adiciona evento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ClassModule } from './modules/class/class.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ClassModule } from "./modules/class/class.module";
 
-import { PeopleModule } from './modules/people/people.module';
-import { UserModule } from './modules/user/user.module';
-import MateriaModule from './modules/materia/materia.module';
+import { PeopleModule } from "./modules/people/people.module";
+import { UserModule } from "./modules/user/user.module";
+import MateriaModule from "./modules/materia/materia.module";
+import EventoModule from "./modules/evento/evento.module";
 
 @Module({
-  imports: [MateriaModule, UserModule, PeopleModule, ClassModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MateriaModule,
+        UserModule,
+        PeopleModule,
+        ClassModule,
+        EventoModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
---
+++ src/modules/evento/evento.controller.ts
@@ -0,0 +1,42 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from "@nestjs/common";
+import EventoService from "./evento.service";
+import CreateEventoDto from "./dto/create-evento.dto";
+import UpdateEventoDto from "./dto/update-evento.dto";
+
+@Controller("subject")
+export default class EventoController {
+    constructor(private readonly subjectService: EventoService) {}
+
+    @Post()
+    create(@Body() createEventoDto: CreateEventoDto) {
+        return this.subjectService.create(createEventoDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.subjectService.findAll();
+    }
+
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        return this.subjectService.findOne(Number(id));
+    }
+
+    @Patch(":id")
+    update(@Param("id") id: string, @Body() updateEventoDto: UpdateEventoDto) {
+        return this.subjectService.update(Number(id), updateEventoDto);
+    }
+
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.subjectService.remove(Number(id));
+    }
+}
